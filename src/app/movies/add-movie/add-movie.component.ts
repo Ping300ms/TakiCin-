73e9370ec58439836f908787d7c8eca371a8de3f
Basile Sens <basile.sens@epita.fr>
2025-04-07 14:38:34 +0200

    ajouté grille pain

--- src/app/movies/add-movie/add-movie.component.ts
+++ src/app/movies/add-movie/add-movie.component.ts
@@ -3,12 +3,16 @@ import {Router} from "@angular/router";
 import {Movie} from "../../models/movie";
 import {FormsModule} from "@angular/forms";
 import {MoviesService} from "../../services/movies.service";
+import {ToastComponent} from "../../toast/toast.component";
+import {ToastService} from "../../services/toast.service";
+import {ToastrService} from "ngx-toastr";
 
 @Component({
   selector: 'app-add-movie',
   standalone: true,
   imports: [
-    FormsModule
+    FormsModule,
+    ToastComponent
   ],
   templateUrl: './add-movie.component.html',
   styleUrl: './add-movie.component.scss'
@@ -26,11 +30,18 @@ export class AddMovieComponent {
     rate: undefined,
     image: undefined
   }
+  show: boolean = false;
+  constructor(private toastr: ToastrService) {}
 
   addMovie():void {
-    if (this.movie.title === '' || this.movie.director === '' || this.movie.synopsis === '') return;
+    if (this.movie.title === '' || this.movie.director === '' || this.movie.synopsis === '') {
+      this.toastr.error('Erreur', 'Remplissez tous les champs avant de quitter');
+      return;
+    }
     console.log("film ajouté");
     this.moviesService.addMovie(this.movie);
-    this.router.navigate(['/movies']);
+    this.router.navigate(['/movies']).then(() => {
+      this.toastr.success('Succès', 'Film ajouté');
+    });
   }
 }
--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,15 +1,17 @@
 import {Component, inject, OnInit} from '@angular/core';
-import {DatePipe} from "@angular/common";
+import {DatePipe, NgForOf} from "@angular/common";
 import {MoviesService} from "../services/movies.service";
 import {Movie} from "../models/movie";
 import {Router, RouterLink} from "@angular/router";
+import {ToastService} from "../services/toast.service";
 
 @Component({
   selector: 'app-movies',
   standalone: true,
   imports: [
     RouterLink,
-    DatePipe
+    DatePipe,
+    NgForOf,
   ],
   templateUrl: './movies.component.html',
   styleUrl: './movies.component.scss'
@@ -17,9 +19,14 @@ import {Router, RouterLink} from "@angular/router";
 export class MoviesComponent{
   private readonly moviesService = inject(MoviesService);
   private readonly router = inject(Router);
+  constructor(protected toastService: ToastService) {}
   movies: Movie[] = this.moviesService.getMovies();
 
   deleteMovie(movie:Movie): void {
     this.moviesService.deleteMovie(movie);
+  }
+
+  removeToast(index:number){
+    this.toastService.remove(index);
   }
 }

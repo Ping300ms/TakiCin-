4757b9f5d75178c3f83042d71e8056cbe5d0c9f3
yann.lemetayer <yann.lemetayer@epita.fr>
2025-04-07 12:45:15 +0000

    feat : login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import {MoviesComponent} from "./movies/movies.component";
 import {AddMovieComponent} from "./movies/add-movie/add-movie.component";
 import {EditMovieComponent} from "./movies/edit-movie/edit-movie.component";
 import {ShowMovieComponent} from "./movies/show-movie/show-movie.component";
+import {LoginComponent} from "./login/login.component";
 
 export const routes: Routes = [
   { path: '', component: HomeComponent},
   { path: 'movies', component: MoviesComponent},
   { path: 'movies/add', component: AddMovieComponent},
   { path: 'movies/edit/:id', component: EditMovieComponent},
-  { path: 'movies/show/:id', component: ShowMovieComponent}
+  { path: 'movies/show/:id', component: ShowMovieComponent},
+  { path: 'login', component: LoginComponent}
 ];
